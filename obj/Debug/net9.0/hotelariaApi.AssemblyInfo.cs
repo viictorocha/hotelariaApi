233cb554d0de579bd8e5583601248638b475ad02
viictorocha <10viictor39@gmail.com>
2026-02-14 11:58:37 -0300

    .

--- obj/Debug/net9.0/hotelariaApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/hotelariaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotelariaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef59a93e107fec96cdbe58ed39fad2ccf249c5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecb0015d0d8cafe75e21b01f87e471da76b563e")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotelariaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotelariaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
